cf5b95352fc01e2b03c411bab6457fa60d9c456c
Aditya <adighate10@gmail.com>
2018-10-13 23:35:16 +0530

    Design initiation

--- numplate/settings.py
+++ numplate/settings.py
@@ -94,15 +94,17 @@ WSGI_APPLICATION = 'numplate.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
+if DEBUG:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
 
-DATABASES = { 'default': dj_database_url.config() }
+else:
+    DATABASES = { 'default': dj_database_url.config() }
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
 
 
 # Password validation
--- product/models.py
+++ product/models.py
@@ -5,9 +5,9 @@ import cloudinary
 from cloudinary.models import CloudinaryField
 from django.db.models.signals import pre_delete
 
-@receiver(pre_delete, sender=Photo)
-def photo_delete(sender, instance, **kwargs):
-    cloudinary.uploader.destroy(instance.image.public_id)
+# @receiver(pre_delete, sender=Photo)
+# def photo_delete(sender, instance, **kwargs):
+#     cloudinary.uploader.destroy(instance.image.public_id)
 
 class Photo(models.Model):
     image = CloudinaryField('image')

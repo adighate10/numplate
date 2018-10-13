951f65d106468ac5814767c50ed19d5149cca642
Aditya <adighate10@gmail.com>
2018-10-13 16:25:18 +0530

    configured heroku and new database

--- numplate/settings.py
+++ numplate/settings.py
@@ -20,10 +20,12 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'j%#rhh02k)sycnr4n4@(ku2db#)!^qy_z)yua)6eup@!0hov-p'
+SECRET_KEY = config('SECRET_KEY')
+# SECRET_KEY = 'j%#rhh02k)sycnr4n4@(ku2db#)!^qy_z)yua)6eup@!0hov-p'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -77,11 +79,17 @@ WSGI_APPLICATION = 'numplate.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 
 # Password validation
